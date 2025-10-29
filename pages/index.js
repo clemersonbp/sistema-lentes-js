00073f62288730d1495345eb4b23751239c5ea4c
clemersonbp <clemersonpessalli@gmail.com>
2025-10-29 20:29:15 -0300

    Atualizacao projeto para nextjs

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Sistema de Lentes</title>
+        <meta name="description" content="Sistema de lentes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <h2>Home</h2>
+      </main>
+    </>
+  );
+}
